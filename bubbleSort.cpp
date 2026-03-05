67f6431a48b47482489914698217427da254d5e2
kalycaokta-ux <kalycaokta@gmail.com>
2026-03-05 11:05:25 +0700

    menambahkan input array

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -8,13 +8,20 @@ int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen
 void input() {       // procedur untuk input
     while (true) {
         cout << "Masukkan banyaknya elemen pada array : ";
-        cin >> n;    // Input dari pengguna
-        if (n <= 20) // Jika n kurang dari atau sama dengan 20
-            break    // keluar dari loop
-        else (       // Jika n lebih dari 20
+        cin >> n;    
+        if (n <= 20) 
+            break;    
+        else {      
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; 
-        )
+        }
     }
     cout << endl;
-    cout << "============"
+    cout << "============" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr [i];
+    }
 }     

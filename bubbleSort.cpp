ab351dd6b836e88157312bc6cfd99ba6675684a2
kalycaokta-ux <kalycaokta@gmail.com>
2026-03-05 11:36:26 +0700

    masukkan display

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -16,9 +16,9 @@ void input() {       // procedur untuk input
         }
     }
     cout << endl;
-    cout << "============" << endl;
+    cout << "=====" << endl;
     cout << "Masukkan Elemen Array" << endl;
-    cout << "============" << endl;
+    cout << "=====" << endl;
 
     for (int i = 0; i < n; i++) {
         cout << "Data ke-" << (i + 1) << ": ";
@@ -48,4 +48,24 @@ void bubbleSortArray() {
         cout << endl;
 
     } while (pass <= n - 1);
+}
+
+
+void display() {
+    cout << endl;
+    cout << "=====" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=====" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j]; 
+        if (j < n - 1) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
 }
